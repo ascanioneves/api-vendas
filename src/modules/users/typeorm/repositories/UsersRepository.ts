0dde2a80ed76fe3b1c825d09b2ca0bd627f7dc31
ascanioneves <ascaniosavio97@gmail.com>
2023-04-22 02:19:02 -0300

    feat: add user custom repository

--- src/modules/users/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -2,7 +2,7 @@ import { EntityRepository, Repository } from 'typeorm';
 import { User } from '../entities/User';
 
 // conexão com a entidade e, consequentemente, com a tabela no banco, podendo assim poder fazer operações
-// diretamente no banco
+// diretamente no banco.
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   public async findByName(name: string): Promise<User | undefined> {
