26000813bc355ed29fbf3d9caf63d22467f2be10
ascanioneves <ascaniosavio97@gmail.com>
2023-04-22 02:18:09 -0300

    feat: add user custom repositorie

---
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,37 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { User } from '../entities/User';
+
+// conexão com a entidade e, consequentemente, com a tabela no banco, podendo assim poder fazer operações
+// diretamente no banco
+@EntityRepository(User)
+export class UsersRepository extends Repository<User> {
+  public async findByName(name: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return user;
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: {
+        email,
+      },
+    });
+
+    return user;
+  }
+}
